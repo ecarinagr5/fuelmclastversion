91692d0a21dbf447801edac6dccf45bfefda12da
Carina Gonzalez Ruiz <5234882-egonzaru@users.noreply.gitlab.com>
2021-01-25 13:27:51 -0600

    CambioS2

--- src/pages/Estacion.js
+++ src/pages/Estacion.js
@@ -517,8 +517,20 @@ resetSimulador(){
               <div className="container-buttons">
                 <Button color="primary" onClick={this.handleClick} className="btn-main-fuel-right">SIMULAR</Button>
                 <Button color="primary" onClick={this.handleClick} className="btn-main-fuel-right">APLICAR</Button>
-                <Button color="primary" onClick={this.toggle('nested_parent')} className="btn-main-fuel-right">ACEPTAR</Button>
-                <Button color="danger" onClick={this.resetSimulador} className="btn-main-fuel-right">CLEAR</Button>
+                <Button color="primary" onClick={this.toggle('nested_parent')} className="btn-main-fuel-right">
+                  ACEPTAR &nbsp;
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar2-event" viewBox="0 0 16 16">
+                  <path d="M11 7.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z"/>
+                  <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2z"/>
+                  <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z"/>
+                </svg>
+                </Button>
+                <Button color="danger" onClick={this.resetSimulador} className="btn-main-fuel-right">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-reply-all-fill" viewBox="0 0 16 16">
+                  <path d="M8.021 11.9L3.453 8.62a.719.719 0 0 1 0-1.238L8.021 4.1a.716.716 0 0 1 1.079.619V6c1.5 0 6 0 7 8-2.5-4.5-7-4-7-4v1.281c0 .56-.606.898-1.079.62z"/>
+                  <path d="M5.232 4.293a.5.5 0 0 1-.106.7L1.114 7.945a.5.5 0 0 1-.042.028.147.147 0 0 0 0 .252.503.503 0 0 1 .042.028l4.012 2.954a.5.5 0 1 1-.593.805L.539 9.073a1.147 1.147 0 0 1 0-1.946l3.994-2.94a.5.5 0 0 1 .699.106z"/>
+                </svg>
+              </Button>
               </div>
               </CardHeader>
         
